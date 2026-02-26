70a7fce91351ab8a9318d4686684871addae800e
Hashi Tarar <hashitarar3@gmail.com>
2026-02-26 12:12:17 +0530

    js

---
+++ Script.js
@@ -0,0 +1,34 @@
+console.log("Hello world")
+
+var a;
+a = "Hashi"
+console.log(a);
+var a = 30;
+console.log(a);
+{
+let name;
+name = "Hashi";
+name = "Hashi Tarar";
+console.log(name);
+}
+let name;
+name="Himanshu singh";
+console.log(name);
+
+const name = "Hashi";
+
+console.log(name);
+console.log(name);
+
+let a = 10;
+let b = 5;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a++);
+console.log(++a);
+console.log(a--);
+console.log(--a);
+
